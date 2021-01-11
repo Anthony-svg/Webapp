886baebf633c2dbadc70f831687ef804f1ce832e
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2021-01-11 13:55:28 -0500

    Update template.php

--- views/template.php
+++ views/template.php
@@ -8,7 +8,7 @@
     <body>
 
         <header>
-        <h1><img src="images/Logo1.jpg"></h1>
+        <h1><img src="images/Logo1.jpg" width="100" height="100"></h1>
         </header> 
 
         <?php
@@ -20,6 +20,13 @@
         $mvc = new MvcController();
         $mvc -> enlacesPaginasController();
         ?>
+
+        <article>
+        <img src="http://havit.ec/wp-content/uploads/2017/08/Magic-Eagle-Headphone-Black-Orange-Content.jpg" width="150" height="150">
+        
+        </article>
+
         </section>
+
     </body>
 </html>
