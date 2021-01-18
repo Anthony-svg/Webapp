2676897ee69009e5d69fbbeac87df7f003367bb8
Ludwing <pansitas.2001@gmail.com>
2021-01-18 17:06:50 -0500

    modulo contactos

--- views/modules/contactanos.php
+++ views/modules/contactanos.php
@@ -8,18 +8,24 @@
 </head>
 
 <body>
-    <header>
-    CONTACTANOS EN NUESTROS SITIOS OFICIALES
-    </header>
 
     <section>
-        aqui va una imagen <br>
-        wttps
-        facebook
-        correo
+        <table alight="center"> 
+            <tr>
+                <th colspan="4" alight="center"> SITIOS OFICIALES </th>
+            </tr>
+
+            <tr>
+                <td rowspan="3"><img src="images/contactanos.png"></td>
+                <td><a href="http://www.facebook.com/" target="_blank"><img src="images/logo_facebook.png" width=32 height=32  /></a> FKGAMER</td>
+                <td><a href="http://www.gmail.com/" target="_blank"><img alt="correo" src="images/correo.png" width=32 height=32  /></a> fkgamer@gamer.com </td>
+                 <td><a href="https://www.google.com/maps/place/Parroquia+La+Merced,+Ambato/@-1.2269399,-78.7426428,11z/data=!4m8!1m2!2m1!1sgoogle+maps!3m4!1s0x91d381bc7458fce1:0xf97d76c9001375b4!8m2!3d-1.2330962!4d-78.6200129" target="_blank"><img alt="visitanos" src="images/ubicacion.png" width=32 height=32  /></a> Av. La Merced</td>
+            </tr>                                           
+        </table>
     </section> 
 
 </body>
 
 
 </html>
+
