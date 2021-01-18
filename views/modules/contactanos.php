6b3017939e7bbd5e88474a9b7d77b2b508e2ba86
Ludwing <pansitas.2001@gmail.com>
2021-01-18 12:26:14 -0500

    conexion y contactanos

--- views/modules/contactanos.php
+++ views/modules/contactanos.php
@@ -1,3 +1,25 @@
-<?php
- echo "estas en contactos";
-?>
+<html>
+<head>
+<meta charset="UTF-8">
+    <title>Contactos</title>
+    <link rel=StyleSheet href="css/contactanos.css" typr="text/css">
+    <link rel=StyleSheet href="bootstrap/css/bootstrap.min.css" typr="text/css">
+    
+</head>
+
+<body>
+    <header>
+    CONTACTANOS EN NUESTROS SITIOS OFICIALES
+    </header>
+
+    <section>
+        aqui va una imagen <br>
+        wttps
+        facebook
+        correo
+    </section> 
+
+</body>
+
+
+</html>
--- views/modules/navigation.php
+++ views/modules/navigation.php
@@ -3,7 +3,7 @@
                 <li> <a href="index.php?action=inicio"> Inicio</a> </li>
                 <li> <a href="index.php?action=nosotros"> Nosotros</a> </li>
                 <li> <a href="index.php?action=servicios"> Servicios</a> </li>
-                <li> <a href="index.php?action=contactanos"> Contactenos</a> </li>
+                <li> <a href="index.php?action=contactanos"> Contactanos</a> </li>
             
             </ul>
 </nav>
