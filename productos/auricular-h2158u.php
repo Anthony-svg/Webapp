79c6f38846778ddec05a7071614d8884e31048cc
Anthony-svg <anthony.melendez07.am@gmail.com>
2021-11-04 11:22:49 -0500

    Cambio de las interfaces de los productos

--- productos/auricular-h2158u.php
+++ productos/auricular-h2158u.php
@@ -4,6 +4,8 @@
 	<meta name="viewport" content="width=device-width, user-scalable = no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no">
     <title>Game Shop</title>
     <link rel=StyleSheet href="../css/productos.css" typr="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
     <body>
@@ -19,36 +21,37 @@
             if($estado){         
                    
                     ?>
-                        <nav>
+                        <nav class="navbar navbar-dark bg-dark">
+                        <div style="color:white;">
+                    <?php 
+                    echo "Bienvenido/a ";
+                    echo $_SESSION['nom'];
+                    ?>
+                    </div>
+    
                         <ul>
-                        <li> <a href="../views\modules\inicio.php"> Inicio</a> </li>
-                        <li> <a href="../views\modules\nosotros.php"> Nosotros</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/inicio.php"> Inicio</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/nosotros.php"> Nosotros</a> </li>
                         
-                        <li> <a href="../views\modules\contactanos.php"> Contactenos</a> </li>
-                        <li> <a href="../views/modules/compras.php">Compras</a> </li>
-                        
-                        <br><br>
-                        <li> <a href="../models\logout.php"> Cerrar sesión</a> </li>
-                       
-                    
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/contactanos.php"> Contactenos</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/compras.php">Compras</a> </li>                                                                      
                     </ul>
+                    <div class="d-flex">
+                            <button class="btn btn-outline-success" onclick="location.href='..\\models\\logout.php'" type="button">Cerrar Sesión</button>
+                        </div>
                 </nav>
             <br><br>
         <?php
-         echo "Bienvenido/a ";
-         echo $_SESSION['nom'];
+     
             }else{
             
                 ?>
-                    <nav>
+                    <nav class="navbar navbar-dark bg-dark">
                     <ul>
-                    <li> <a href="../views\modules\inicio.php"> Inicio</a> </li>
-                    <li> <a href="../views\modules\nosotros.php"> Nosotros</a> </li>
-                    <li> <a href="../views\modules\contactanos.php"> Contactenos</a> </li>
-                    <br><br>
-                    <li> <a href="../views/modules/login.php"> Iniciar sesión</a> </li>
-                   
-                
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/inicio.php"> Inicio</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/nosotros.php"> Nosotros</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/contactanos.php"> Contactenos</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/login.php"> Iniciar sesión</a> </li>                                   
                 </ul>
             </nav>
 
@@ -60,6 +63,7 @@
 		<img class="izquierda" src="../images/Producto1.jpg" >
 <aside>AURICULAR GAMING HAVIT HV-H2158U CONEXIÓN USB 7.1 
 	<br>
+    
 	<br>		
 				Tipo:Over Ear / Circumaurales<br>
 				Conectividad:	Alámbrico<br>
@@ -70,8 +74,24 @@
 				Tipo de material:	Plástico<br>
 				Componentes incluidos:	Audio (3,5 mm)<br>
 				Tipo de contector:	Auxiliar jack 3.5mm<br>
-				Respuesta de frequencia:	20Hz – 20Khz</aside>            
+				Respuesta de frequencia:	20Hz – 20Khz
+                
+            </aside>
+       
+            
+  <div class="row">
+
+    <div class="col-auto">                     
+            <?php
+            if (!empty($_SESSION['nom'])) {?>
+           <button class="btn btn-primary btn-lg" onclick="location.href='..\\views\\modules\\compras.php'" type="button">Comprar Producto</button>         
+            <?php
+            }else{
+            }
+            ?>   
+    </div>
+  </div>     
 				<div class="reset"></div>
-				</div>
+				</div>                    
     </body>
 </html>    
--- productos/mouse-ms783.php
+++ productos/mouse-ms783.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <title>Game Shop</title>
     <link rel=StyleSheet href="../css/productos.css" typr="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
     <body>
@@ -17,36 +19,37 @@
             if($estado){         
                    
                     ?>
-                        <nav>
+                        <nav class="navbar navbar-dark bg-dark">
+                        <div style="color:white;">
+                    <?php 
+                    echo "Bienvenido/a ";
+                    echo $_SESSION['nom'];
+                    ?>
+                    </div>
+    
                         <ul>
-                        <li> <a href="../views\modules\inicio.php"> Inicio</a> </li>
-                        <li> <a href="../views\modules\nosotros.php"> Nosotros</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/inicio.php"> Inicio</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/nosotros.php"> Nosotros</a> </li>
                         
-                        <li> <a href="../views\modules\contactanos.php"> Contactenos</a> </li>
-                        <li> <a href="../views/modules/compras.php">Compras</a> </li>
-                        
-                        <br><br>
-                        <li> <a href="../models\logout.php"> Cerrar sesión</a> </li>
-                       
-                    
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/contactanos.php"> Contactenos</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../views/modules/compras.php">Compras</a> </li>                                                                      
                     </ul>
+                    <div class="d-flex">
+                            <button class="btn btn-outline-success" onclick="location.href='..\\models\\logout.php'" type="button">Cerrar Sesión</button>
+                        </div>
                 </nav>
             <br><br>
         <?php
-         echo "Bienvenido/a ";
-         echo $_SESSION['nom'];
+     
             }else{
             
                 ?>
-                    <nav>
+                    <nav class="navbar navbar-dark bg-dark">
                     <ul>
-                    <li> <a href="../views\modules\inicio.php"> Inicio</a> </li>
-                    <li> <a href="../views\modules\nosotros.php"> Nosotros</a> </li>
-                    <li> <a href="../views\modules\contactanos.php"> Contactenos</a> </li>
-                    <br><br>
-                    <li> <a href="../views/modules/login.php"> Iniciar sesión</a> </li>
-                   
-                
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/inicio.php"> Inicio</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/nosotros.php"> Nosotros</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/contactanos.php"> Contactenos</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="../views/modules/login.php"> Iniciar sesión</a> </li>                                   
                 </ul>
             </nav>
 
@@ -71,7 +74,19 @@ Aceleración máx.: >20 G2Pruebas realizadas sobre alfombrilla de mouse para jue
 Velocidad máx.: >150 ips3Pruebas realizadas sobre alfombrilla de mouse para juegos<br>
 Formato de datos USB: 16 bits/eje<br>
 Velocidad de respuesta USB: 1000 Hz (1 ms)<br>
-Microprocesador: 32 bits ARM</aside>            
+Microprocesador: 32 bits ARM</aside>
+<div class="row">
+
+<div class="col-auto">                     
+        <?php
+        if (!empty($_SESSION['nom'])) {?>
+       <button class="btn btn-primary btn-lg" onclick="location.href='..\\views\\modules\\compras.php'" type="button">Comprar Producto</button>         
+        <?php
+        }else{
+        }
+        ?>   
+</div>
+</div>             
 				<div class="reset"></div>
 				</div>
     </body>
